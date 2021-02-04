41411595c07182b2fa80d917b7ea9c249dbdcaaf
Daniel Hindrikes <danielhindrikes@daniels-m1.lan>
2021-02-04 12:06:59 +0100

    inital commit

---
+++ TackleStore.Clients/Services/CategoriesService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TackleStore.Models;
+
+namespace TackleStore.Clients.Services
+{
+    public class CategoriesService
+    {
+        public async static Task<List<Category>> GetCategories()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://tacklestore.azurewebsites.net/api/categories");
+
+            if(!response.IsSuccessStatusCode)
+            {
+                return new List<Category>();
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            var result = JsonSerializer.Deserialize<List<Category>>(json, new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            return result;
+        }
+    }
+}
